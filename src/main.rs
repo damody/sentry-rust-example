ed22c59259fd31659ca08da50c50a76c759aebef
damody <liam.huang@4ipnet.com>
2018-07-27 10:24:58 +0800

    change backtrace current_stacktrace to utils current_stacktrace  Changes to be committed: 	modified:   Cargo.lock 	modified:   Cargo.toml 	modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,24 @@
-#![feature(rust_2018_preview)]
-#[warn(unused_imports)]
-
 use std::error::Error;
 
 extern crate sentry;
 use sentry::integrations::failure::capture_error;
 use sentry::integrations::panic::register_panic_handler;
 use sentry::protocol::*;
-
-use sentry::backtrace_support::current_stacktrace;
+use sentry::utils::current_stacktrace;
 
 fn test() {
     sentry::capture_event(Event {
-        message: Some("Hello test!".into()),
+        message: Some("Hello test!2".into()),
         level: Level::Info,
         stacktrace: current_stacktrace(),
         ..Default::default()
     });
 }
 fn main() {
-    let _guard = sentry::init("https://key@sentry.io/1248884");
+    let _guard = sentry::init("https://d04981d09005480bb62db4ee3d02138e@sentry.io/1248884");
     sentry::capture_message("Hello World!", sentry::Level::Info);
     register_panic_handler();
     test();
     // code using sentry goes here.
-    sentry::capture_event(Event {
-        message: Some(format!("{} {}\n{}", file!(), line!(), format!("{:?}", Backtrace::new())).into()),
-        level: Level::Info,
-        ..Default::default()
-    });
     panic!("hello world");
 }
